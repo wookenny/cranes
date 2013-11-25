50cacba4288998a9f57b15d1073207d07274e24d
Torsten Gellert <t.gellert@gmail.com>
2013-11-25 14:08:44 +0100

    version tracking in compilation

---
+++ build_version.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DIR='./'
+count=`git rev-list HEAD | wc -l | sed -e 's/ *//g' | xargs -n1 printf %04d`
+commit=`git show --abbrev-commit HEAD | grep '^commit' | sed -e 's/commit //'`
+buildno=build."$count.$commit"
+
+echo $buildno > "version.txt"
