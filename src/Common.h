d7119f6527d3cf1343b061fe95528a426c5c4683
Torsten Gellert <t.gellert@gmail.com>
2014-03-17 14:02:02 +0100

    COmmon is compliant with my code styleguide

--- src/Common.h
+++ src/Common.h
@@ -6,8 +6,8 @@
 
 std::vector<std::string> split(const std::string &s, char delim);
 
-//replaces all occureances of from in str with to
-void replaceAll(std::string& str, const std::string& from, 
+// replaces all occureances of from in str with to
+void replaceAll(std::string& str, const std::string& from,
                                   const std::string& to);
 
 // split string at every whitespace position
@@ -16,23 +16,25 @@ std::vector<std::string> split(const std::string &input);
 // convert a duration object into a nice and readable string, represeting
 // the same duration
 template<class Duration>
-std::string duration_to_string(const Duration& dtn){
+std::string duration_to_string(const Duration& dtn) {
     std::string dur;
     auto h = std::chrono::duration_cast<std::chrono::hours>(dtn).count();
-    if(h>0)
+    if (h > 0)
         dur += std::to_string(h)+" hours";
     auto m = std::chrono::duration_cast<std::chrono::minutes>(dtn).count();
     m -= h*60;
-    if(m>0)
-        dur += (h>0?" ":"")+std::to_string(m)+" minutes";
+    if (m > 0)
+        dur += (h > 0 ?" ":"")+std::to_string(m)+" minutes";
     auto s = std::chrono::duration_cast<std::chrono::seconds>(dtn).count();
     s -= (h*60*60 + m*60);
-    if(s>0)  dur += ( (h>0 or m > 0) ? " " : "")+std::to_string(s)+" seconds";    
+    if (s > 0)
+        dur += ( (h > 0 or m > 0) ? " " : "")+std::to_string(s)+" seconds";
+
     return dur;
 }
 
 // used to build intervals for the batch mode
-//exampels:  
+// exampels:
 // * "a-d"   -> {a,b,c,d}
 // * "[a,d]" -> {a,b,c,d}
 // * "a"     -> {a}
@@ -40,6 +42,6 @@ std::string duration_to_string(const Duration& dtn){
 std::vector<std::string> create_interval(const std::string&);
 
 // used to build find files recursively
-//  E.g.: "./f*/g*.png" find all files with name g*.png   
+// E.g.: "./f*/g*.png" find all files with name g*.png
 // in folders starting with f in the current directory
 std::vector<std::string> find_files(const std::string&);
--- src/Common_Unittest.cpp
+++ src/Common_Unittest.cpp
@@ -1,64 +1,63 @@
 #include <gtest/gtest.h>
-#include "Common.h"
+#include <vector>
+#include <string>
+#include "./Common.h"
 
-using namespace std;
+using std::vector;
+using std::string;
 
 TEST(Common_Methods /*Testcase Name*/,Split_Function /*Test name*/) {
-    string str 		= "a,b,v ,d , ,s";
-    vector<string> result1	= {"a","b","v ","d "," ","s"};
-    auto split_result1 	= split(str,',');
-    EXPECT_EQ(result1,split_result1);
-    
-    vector<string> result2	= {"a,b,v ,d , ,s"};
-    EXPECT_EQ( split(str,'-'), result2 );
-    
-    vector<string> result3	= {"a,b,"," ,d , ,s"};
-    EXPECT_EQ( split(str,'v'), result3 );   
+    string str = "a,b,v ,d , ,s";
+    vector<string> result1 = {"a", "b", "v ", "d ", " ", "s"};
+    auto split_result1 = split(str, ',');
+    EXPECT_EQ(result1, split_result1);
 
-    vector<string> result4	= {"a,b,v",",d",",",",s"};
-    EXPECT_EQ( split(str,' '), result4 );   
-    EXPECT_EQ( split(str), result4 );  
-} 
+    vector<string> result2 = {"a,b,v ,d , ,s"};
+    EXPECT_EQ(split(str, '-'), result2);
+
+    vector<string> result3 = {"a,b,", " ,d , ,s"};
+    EXPECT_EQ(split(str, 'v'), result3);
+
+    vector<string> result4 = {"a,b,v", ",d", ",", ",s"};
+    EXPECT_EQ(split(str, ' '), result4);
+    EXPECT_EQ(split(str), result4);
+}
 
 TEST(Common_Methods /*Testcase Name*/,Interval_Function /*Test name*/) {
-
-    
     // * "a-d"   -> {a,b,c,d} AND
     // * "[a,d]" -> {a,b,c,d}
     typedef vector<string> INTER;
-    INTER ints1a =  create_interval("-3-6"); 
-    INTER ints1b = {"-3","-2","-1","0","1","2","3","4","5","6"};
-    EXPECT_EQ( ints1a, ints1b);  
-    EXPECT_EQ( create_interval("[-3,6]"), ints1b);      
-    
-    INTER ints2a =  create_interval("-4--1"); 
-    INTER ints2b = {"-4","-3","-2","-1"};
-    EXPECT_EQ( ints2a, ints2b);
-    EXPECT_EQ( create_interval("[-4,-1]"), ints2b);   
-       
-    INTER ints3a =  create_interval("4--1"); 
+    INTER ints1a = create_interval("-3-6");
+    INTER ints1b = {"-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6"};
+    EXPECT_EQ(ints1a, ints1b);
+    EXPECT_EQ(create_interval("[-3,6]"), ints1b);
+
+    INTER ints2a =  create_interval("-4--1");
+    INTER ints2b = {"-4", "-3", "-2", "-1"};
+    EXPECT_EQ(ints2a, ints2b);
+    EXPECT_EQ(create_interval("[-4,-1]"), ints2b);
+
+    INTER ints3a =  create_interval("4--1");
     INTER ints3b = {};
-    EXPECT_EQ( ints3a, ints3b);
-    EXPECT_EQ( create_interval("[4,-1]"), ints3b);  
-    
-    INTER ints4a =  create_interval("-4--5"); 
+    EXPECT_EQ(ints3a, ints3b);
+    EXPECT_EQ(create_interval("[4,-1]"), ints3b);
+
+    INTER ints4a =  create_interval("-4--5");
     INTER ints4b = {};
-    EXPECT_EQ( ints4a, ints4b);
-    EXPECT_EQ( create_interval("[-4,-5]"), ints4b);  
-        
-    INTER ints5a =  create_interval("4-2"); 
+    EXPECT_EQ(ints4a, ints4b);
+    EXPECT_EQ(create_interval("[-4,-5]"), ints4b);
+
+    INTER ints5a = create_interval("4-2");
     INTER ints5b = {};
-    EXPECT_EQ( ints5a, ints5b);
-    EXPECT_EQ( create_interval("[4,2]"), ints5b);      
-      
-    
+    EXPECT_EQ(ints5a, ints5b);
+    EXPECT_EQ(create_interval("[4,2]"), ints5b);
+
     // * "a"     -> {a}
     INTER int6 = {"213"}; INTER int7 = {"2"};
     EXPECT_EQ(int6, create_interval("213"));
     EXPECT_EQ(int7, create_interval("2"));
-    
-    
+
     // * "a,b,d" -> {a,b,d}
-    INTER int9 = {"2","-5","7","0"};
+    INTER int9 = {"2", "-5", "7", "0"};
     EXPECT_EQ(int9, create_interval("2,-5,7,0"));
-} 
+}
